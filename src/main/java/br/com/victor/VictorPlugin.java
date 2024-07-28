6dc89f43622542c6442ec4d6a0786063a6ed8f2d
Victor Mussoline <victor8augusto010604@hotmail.com>
2024-07-28 19:16:08 -0300

    Testes Java
    Fazendo teste de criação, instalação e desenvolvimento de plugins para Minecraft utilizando Java

---
+++ src/main/java/br/com/victor/VictorPlugin.java
@@ -0,0 +1,17 @@
+package br.com.victor;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VictorPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
